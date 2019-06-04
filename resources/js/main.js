050830e2ee1c3b34bd0af82c672af3207b505968
Ryan DiCenzo <dicenzoryan@gmail.com>
2019-06-03 17:24:51 -0700

    Add persistence

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,5 +1,3 @@
-
-
 // CLOCK
 
 function setTime() {
@@ -20,9 +18,39 @@ setTime()
 
 // TASKS
 
+// 
+var separator = '•';
+
 // SVG Code
 var removeSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"> <rect class="noFill" width="22" height="22"/><g><g><path class="fill" d="M16.1,3.6h-1.9V3.3c0-1.3-1-2.3-2.3-2.3h-1.7C8.9,1,7.8,2,7.8,3.3v0.2H5.9c-1.3,0-2.3,1-2.3,2.3v1.3c0,0.5,0.4,0.9,0.9,1v10.5c0,1.3,1,2.3,2.3,2.3h8.5c1.3,0,2.3-1,2.3-2.3V8.2c0.5-0.1,0.9-0.5,0.9-1V5.9C18.4,4.6,17.4,3.6,16.1,3.6z M9.1,3.3c0-0.6,0.5-1.1,1.1-1.1h1.7c0.6,0,1.1,0.5,1.1,1.1v0.2H9.1V3.3z M16.3,18.7c0,0.6-0.5,1.1-1.1,1.1H6.7c-0.6,0-1.1-0.5-1.1-1.1V8.2h10.6L16.3,18.7L16.3,18.7z M17.2,7H4.8V5.9c0-0.6,0.5-1.1,1.1-1.1h10.2c0.6,0,1.1,0.5,1.1,1.1V7z"/></g><g><g><path class="fill" d="M11,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6s0.6,0.3,0.6,0.6v6.8C11.6,17.7,11.4,18,11,18z"/></g><g><path class="fill" d="M8,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8C7.4,10.2,7.7,10,8,10c0.4,0,0.6,0.3,0.6,0.6v6.8C8.7,17.7,8.4,18,8,18z"/></g><g><path class="fill" d="M14,18c-0.4,0-0.6-0.3-0.6-0.6v-6.8c0-0.4,0.3-0.6,0.6-0.6c0.4,0,0.6,0.3,0.6,0.6v6.8C14.6,17.7,14.3,18,14,18z"/></g></g></g></svg>'
 var completeSVG = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" style="enable-background:new 0 0 22 22;" xml:space="preserve"><rect class="noFill" width="22" height="22"/><g><path class="fill" d="M9.7,14.4L9.7,14.4c-0.2,0-0.4-0.1-0.5-0.2l-2.7-2.7c-0.3-0.3-0.3-0.8,0-1.1s0.8-0.3,1.1,0l2.1,2.1l4.8-4.8c0.3-0.3,0.8-0.3,1.1,0s0.3,0.8,0,1.1l-5.3,5.3C10.1,14.3,9.9,14.4,9.7,14.4z"/></g></svg>'
+
+var tasksText = ''
+chrome.storage.sync.get('tasks', function (items) {
+  loadTasksFromText(items.tasks)
+});
+
+console.log(tasksText)
+loadTasksFromText(tasksText)
+
+function loadTasksFromText(text) {
+
+  var lastTaskEndPos = 0;
+
+  for (var i = 0; i < text.length; i++) {
+    if (text.charAt(i) == separator) {
+      var newTask = text.substring(lastTaskEndPos, i);
+      addItemTodo(newTask);
+      lastTaskEndPos = i + 1;
+    }
+  }
+}
+
+
+function updateSyncedTasks(newTasks) {
+  tasksText = newTasks;
+  chrome.storage.sync.set({ "tasks": newTasks }, function () { });
+}
 
 // User clicked add button
 // If text inside, add text to list
@@ -38,7 +66,7 @@ document.getElementById('add').addEventListener('click', function() {
 function removeItem() {
   var item = this.parentNode.parentNode;
   var parent = item.parentNode;
-
+  updateSyncedTasks(tasksText.replace(item.innerText + separator, ""));
   parent.removeChild(item)
 }
 
@@ -55,6 +83,7 @@ function completeItem() {
 
   parent.removeChild(item);
   target.insertBefore(item, target.childNodes[0]);
+  updateSyncedTasks(tasksText.replace(item.innerText + separator, ""));
 }
 
 function addItemTodo(text) {
@@ -80,11 +109,11 @@ function addItemTodo(text) {
   // Add click event for completing
   complete.addEventListener('click', completeItem)
 
-
-
   buttons.appendChild(remove);
   buttons.appendChild(complete);
   item.appendChild(buttons);
 
   list.insertBefore(item, list.childNodes[0])
+
+  updateSyncedTasks(tasksText + text + separator)
 }
